0314934a284cbd35efa8fdddf65480437b931274
winderfelipez <winder.felipezz@gmail.com>
2022-12-15 15:52:33 -0300

    CLI and setup.py updated

--- acppred/predict.py
+++ acppred/predict.py
@@ -1,10 +1,28 @@
 from acppred.models import Model
+from argparse import ArgumentParser
+from Bio import SeqIO
+import pandas as pd
 
 def main ():
-    model = Model.load('data/models/model.pickle')
-    sequence = input('peptide sequences:')
-    prediction = model.predict(sequence)
-    print(prediction)
+    argument_parser=ArgumentParser(description= 'predicts anticancer ppetides in a FASTA file')
+    argument_parser.add_argument('--input', required=True, help="Input FASTA file")
+    argument_parser.add_argument('--output', required=True, help='output CSV file')
+    argument_parser.add_argument('--model',required=True, help='Pres-trained ACPPred model')
+    
+    arguments= argument_parser.parse_args()
+    
+    model = Model.load(arguments.model)
+    predictions=[]
+
+    for sequence_record in SeqIO.parse(arguments.input, 'fasta'):
+        sequence= str(sequence_record.seq)
+        prediction=model.predict(sequence)
+        sequence_data={'sequence_id': sequence_record.id, 'prediction': prediction}
+        predictions.append(sequence_data)
+
+    df_predictions= pd.DataFrame(predictions, columns=('sequence_id','prediction' ))
+    df_predictions.to_csv(arguments.output, index=False)
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
- name = 'acppred',
- version = '0.0.1',
- packages= find_packages(),
- author='Frederico Schmill Kremer'
+  name = 'acppred',
+  version = '0.0.1',
+  packages= find_packages(),
+  author='Frederico Schmill Kremer',
+  entry_points = {
+    'console_scripts': [
+        'acppred-train = acppred.train:main',
+        'acppred-predict=acppred.predic:main'
+   ]
+ }
 )
 
 
